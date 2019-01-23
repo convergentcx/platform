80fb9cdae35c2764b75a7162f2bc04687b2663ed
Logan Saether <lsaether@protonmail.com>
2019-01-23 22:17:36 +0100

    launch button

--- src/App.js
+++ src/App.js
@@ -359,19 +359,26 @@ const HomeContainer = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
+  background: ${colors.bgGrey};
 `;
 
 const LaunchButton = styled.button`
-  width: 25%;
-  height: 25%;
+  width: 40vw;
+  height: 40vw;
+  border-radius: 20vw;
   font-size: 48px;
-  background: #2424D0;
-  color: white;
-  border: none;
+  background: #FFF;
+  color: #000;
+  border: solid;
+  border-color: #000;
+  border-width: 16px;
   transition: 1s;
+  font-style: italic;
+  ${shadowMixin}
   :hover {
-    color: orange;
-    background: #05021A;
+    color: #FFF;
+    background: #000;
+    box-shadow: 0 15px 35px rgba(50,50,93,.9), 0 5px 15px rgba(0,0,0,.87);
   }
 `;
 
@@ -882,13 +889,38 @@ const ProfilePage = withRouter((props) => (
   </ProfileContainer>
 ));
 
-const HomePage = () => (
-  <HomeContainer>
-    <LaunchButton>
-      Launch Your Token
-    </LaunchButton>
-  </HomeContainer>
-);
+class HomePage extends React.Component {
+  state = {
+    text: 'CONVERGENT'
+  }
+
+  mouseEnter = () => {
+    this.setState({ text: '' });
+    setTimeout(() => {
+      this.setState({ text: 'LAUNCH' })
+    }, 600);
+  }
+
+  mouseExit = () => {
+    this.setState({ text: '' });
+    setTimeout(() => {
+      this.setState({ text: 'CONVERGENT' })
+    }, 300);
+  }
+
+  render() {
+    return (
+      <HomeContainer>
+        <LaunchButton
+          onMouseEnter={this.mouseEnter}
+          onMouseLeave={this.mouseExit}
+        >
+          {this.state.text}
+        </LaunchButton>
+      </HomeContainer>
+    );
+  }
+};
 
 const FAQPage = () => (
   <FAQContainer>
